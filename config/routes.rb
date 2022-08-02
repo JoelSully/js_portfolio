53b163af4316fa97a8dd946cf569080b50b76530
Joel <joel@Amandas-MacBook-Air.local>
2022-08-01 19:35:23 -0500

    added root and new to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :posts
+  get 'posts/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root 'home#index'
+  root 'posts#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
