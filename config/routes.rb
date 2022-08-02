103039bab136625cfc984557b4f3d11fdba4bfe8
Joel <joel@Amandas-MacBook-Air.local>
2022-08-02 17:17:46 -0500

    changed root back

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :posts
   get 'posts/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root 'home#index'
+  root 'posts~~#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
